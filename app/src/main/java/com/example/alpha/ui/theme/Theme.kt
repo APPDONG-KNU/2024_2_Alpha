37f586da3bfc6ad6b7c5f31dcf89499e93c9b1b0
ijh1298 <imjh0417@gmail.com>
2024-10-08 11:31:11 +0900

    Fix: 프로젝트 누락 파일 재설정

--- app/src/main/java/com/example/alpha/ui/theme/Theme.kt
+++ app/src/main/java/com/example/alpha/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MyApplicationTheme(
+fun AlphaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
