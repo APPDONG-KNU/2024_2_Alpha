51cb6efcc4ad38f0d4be7c39f6169457807e18bf
ijh1298 <imjh0417@gmail.com>
2024-11-04 21:20:54 +0900

    Feat: ScheduleCard 컴포저블 추가

--- app/src/main/java/com/example/alpha/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/example/alpha/schedule/ScheduleScreen.kt
@@ -2,26 +2,30 @@ package com.example.alpha.schedule
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.alpha.R
@@ -30,7 +34,6 @@ import com.example.alpha.data.model.dummySchedules
 
 @Composable
 fun ScheduleScreen() {
-    val dummies =
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,16 +67,37 @@ fun ScheduleCard(
                 painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = null,
                 modifier = Modifier
-                    .height(100.dp),
+                    .size(40.dp)
+                    .clip(CircleShape),
                 contentScale = ContentScale.Crop
             )
-            Column {
-                Text(schedule.title, style = MaterialTheme.typography.titleSmall)
-                Text(schedule.content, style = MaterialTheme.typography.bodyMedium)
+            Column(
+                modifier = Modifier.padding(start = 16.dp)
+            ) {
+                Text(
+                    schedule.title,
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Row(
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = schedule.location + " · ",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Text(
+                        text = schedule.date,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
             }
         }
     }
 }
+
 @Preview
 @Composable
 fun ScheduleCardPreview() {
