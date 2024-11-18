870ee773fe7178207027362b4afd510a5c781ef5
ijh1298 <imjh0417@gmail.com>
2024-11-18 14:51:26 +0900

    Chore: ScheduleCard 디자인 수정

--- app/src/main/java/com/example/alpha/schedule/ScheduleCard.kt
+++ app/src/main/java/com/example/alpha/schedule/ScheduleCard.kt
@@ -4,15 +4,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.alpha.R
@@ -26,25 +36,40 @@ fun ScheduleCard(
 ) {
     Card(
         modifier = Modifier
-            .padding(4.dp)
+            .padding(16.dp)
             .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(4.dp),
         shape = RoundedCornerShape(12.dp)
     ) {
         Row(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_background),
                 contentDescription = null,
                 modifier = Modifier
-                    .height(100.dp),
+                    .clip(CircleShape)
+                    .size(40.dp),
                 contentScale = ContentScale.Crop
             )
-            Column {
-                Text(schedule.title, style = MaterialTheme.typography.titleSmall)
-                Text(schedule.content, style = MaterialTheme.typography.bodyMedium)
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .weight(1f)
+            ) {
+                Text(schedule.title, style = MaterialTheme.typography.titleSmall, fontWeight = FontWeight.SemiBold)
+                Row {
+                    Text(schedule.location, style = MaterialTheme.typography.bodyMedium)
+                    Text(" · ", style = MaterialTheme.typography.bodyMedium)
+                    Text(schedule.date, style = MaterialTheme.typography.bodyMedium)
+                }
             }
+            Icon(
+                Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Right Arrow"
+            )
         }
     }
 }
